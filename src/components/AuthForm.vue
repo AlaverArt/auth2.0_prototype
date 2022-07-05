47e4c0f9a0910c7c85880dc5879a8da80bc3eccd
alaverArt <alaverArt@yandex.ru>
2022-07-05 22:26:26 +0300

    init commit

--- src/components/AuthForm.vue
+++ src/components/AuthForm.vue
@@ -1,10 +1,20 @@
 <template>
-$END$
+  <div>
+    <form>
+      <fieldset>
+        <legend>Авторизация</legend>
+        <p><label for="login">Имя <em>*</em></label><input type="text" id="login"></p>
+        <p><label for="password">E-mail</label><input type="password" id="password"></p>
+      </fieldset>
+      <p><input type="submit" value="Войти"></p>
+      <p></p>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-name: "AuthForm"
+  name: "AuthForm"
 }
 </script>
 
